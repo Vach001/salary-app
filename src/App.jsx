9b28b8f1e1a82da0eb4889f14b6db3c40a742069
Vache Apinyan <apinyan.v@gmail.com>
2023-07-08 23:20:08 +0400

    feat(*): clean app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.css';
+
+function App() {
+  return (
+    <div className={styles.app}>
+    " hello react"
+    </div>
+  );
+}
+
+export default App;
