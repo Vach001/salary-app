379029796ec12b4c9185c16dc870436e4ac5e67f
Vache Apinyan <apinyan.v@gmail.com>
2023-07-15 14:44:36 +0400

    feat(Salary): add Salary and Header components

---
+++ src/Components/Salary/Salary.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Header/Header'
+import InputButtons from '../InputButtons/InputButtons'
+
+export default function Salary() {
+  return (
+    <>
+        <Header />
+        <InputButtons />
+    </>
+  )
+}
