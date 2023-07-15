484e321e8000d5b8706b6c3e3e8aa750eedcff69
Vache Apinyan <apinyan.v@gmail.com>
2023-07-15 13:46:20 +0400

    feat(*): add nextUI package, add idea to gitignore change App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from './App.css';
 
 function App() {
   return (
-    <div className={styles.app}>
-    Hello React
+    <div>
+    Hello Salary App
     </div>
   );
 }
