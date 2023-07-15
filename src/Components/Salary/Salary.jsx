151697395d124c05bb0f0882b099bbc31686b74b
Vache Apinyan <apinyan.v@gmail.com>
2023-07-15 20:24:48 +0400

    feat(*): add InputButton and PensionButton components and add other styles

--- src/Components/Salary/Salary.jsx
+++ src/Components/Salary/Salary.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from '../Header/Header'
 import InputButtons from '../InputButtons/InputButtons'
+import PensionButtons from '../PensionButtons/PensionButtons'
+import { Card } from '@nextui-org/react'
+import styles from "./Salary.module.css"
 
 export default function Salary() {
   return (
     <>
         <Header />
-        <InputButtons />
+        <Card className={styles.calcForm}>            
+            <InputButtons />
+            <PensionButtons />
+        </Card>
     </>
   )
 }
