9ab81b2dbdfb2f2ec6d05fe5daf719c377322d29
RafaelDallaqyan2000 <rafaeldallakyan4@gmail.com>
2023-07-16 23:43:09 +0400

    add material ui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Salary from "./Components/Salary/Salary";
+import { Box } from "@mui/material";
 
 function App() {
   return (
-    <>
+    <Box>
       <Salary />
-    </>
+    </Box> 
   );
 }
 
