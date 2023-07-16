a426e6a19d3e5ce0471cf471b43b71ad391d77bd
Vache Apinyan <apinyan.v@gmail.com>
2023-07-16 14:06:11 +0400

    feat(*): add income tax

---
+++ src/Components/IncomeTax/IncomeTax.jsx
@@ -0,0 +1,26 @@
+import { Card, Input, Text } from "@nextui-org/react";
+import React from "react";
+import {Table} from "@nextui-org/react";
+import styles from "./IncomeTax.module.css"
+
+export default function IncomeTax() {
+  // return (
+  //   // <Table css={{display:"table", width:"100%"}}>
+  //   //     <Table css={{
+  //   //         width:"40%",
+           
+            
+  //   //         }}>
+  //   //         {/* Եկամտային հարկ 20% / ՏՏ 10% */}
+  //   //     </Table>
+  //   //     <Table css={{
+  //   //             width:"60%",
+  //   //             // textAlign:"right",
+  //   //             // paddingRight:"1%"
+  //   //             }}
+  //   //             >
+  //   //         {/* 0 */}
+  //   //     </Table>
+  //   // </Table>
+  // );
+}
