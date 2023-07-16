ff31afd7f88672c648d8603b7145f8e001c85192
RafaelDallaqyan <rafaeldallakyan2000@gmail.com>
2023-07-16 13:39:06 +0400

    news

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './App.css';
 
 function App() {
   return (
-    <div>
-    Hello Salary App
+    <div className={styles.app}>
+    Hello React
     </div>
   );
 }
