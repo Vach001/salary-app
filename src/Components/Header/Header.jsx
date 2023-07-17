95251f8dc155cb1fd99e15c7f1760b7ed383593f
RafaelDallaqyan <rafaeldallakyan2000@gmail.com>
2023-07-17 18:09:39 +0400

    add material ui

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,13 @@
 import React from "react";
-import { Card, Row, Text } from "@nextui-org/react";
+import {Box, Typography} from "@mui/material";
+import styles from './header.module.css';
 
 export default function Header() {
   return (
-    <>
-      <Card css={{ $$cardColor: "$colors$primaryLight" }}>
-        <Card.Body>
-          <Row justify="center" align="center">
-            <Text h1 color="white" css={{m: "$15sl"}} align="center">
-              ԱՇԽԱՏԱՎԱՐՁԻ ՀԱՇՎԻՉ ԲՈԼՈՐԻ ՀԱՄԱՐ
-            </Text>
-          </Row>
-        </Card.Body>
-      </Card>
-    </>
+    <Box className={styles.container}>
+      <Typography variant={"h5"} className={styles.text}>
+        ԱՇԽԱՏԱՎԱՐՁԻ ՀԱՇՎԻՉ ԲՈԼՈՐԻ ՀԱՄԱՐ
+      </Typography>
+    </Box>
   );
 }
--- src/Components/InputButtons/InputButtons.jsx
+++ src/Components/InputButtons/InputButtons.jsx
@@ -1,23 +1,28 @@
-import { Grid, Button } from '@nextui-org/react'
 import React from 'react'
+import {v4} from "uuid";
+import {Box, Button} from "@mui/material";
 import styles from "./InputButtons.module.css"
 
 
+const inputTypes = [
+    {title: 'Իմացեք մաքուր աշխատավարձը', id: v4()},
+    {title: 'Իմացեք գրանցված աշխատավարձը', id: v4()},
+]
+
 export default function InputButtons() {
   return (
-    <>
-        <Grid.Container className={styles.input}>
-            <Grid>
-                <Button color="primary" auto ghost>
-                    Իմացեք մաքուր աշխատավարձը
+    <Box>
+        {
+            inputTypes.map(e => (
+                <Button
+                    variant={"contained"}
+                    className={styles.button}
+                    id={e.id}
+                >
+                    {e.title}
                 </Button>
-            </Grid>
-            <Grid>
-                <Button color="primary" auto ghost>
-                    Իմացեք գրանցված աշխատավարձը
-                </Button>
-            </Grid>    
-        </Grid.Container>
-    </>
+            ))
+        }
+    </Box>
   )
 }
