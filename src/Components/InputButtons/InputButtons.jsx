473efd3bc69b72e35748e47c94c2d4ea83f737f8
RafaelDallaqyan <rafaeldallakyan2000@gmail.com>
2023-07-18 21:30:47 +0400

    change to material ui

--- src/Components/InputButtons/InputButtons.jsx
+++ src/Components/InputButtons/InputButtons.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import {v4} from "uuid";
-import {Box, Button} from "@mui/material";
+import React, { useState } from 'react'
+import { v4 } from "uuid";
+import { Box, Button } from "@mui/material";
 import styles from "./InputButtons.module.css"
 
 
 const inputTypes = [
-    {title: 'Իմացեք մաքուր աշխատավարձը', id: v4()},
-    {title: 'Իմացեք գրանցված աշխատավարձը', id: v4()},
+    { title: 'Իմացեք մաքուր աշխատավարձը', id: v4() },
+    { title: 'Իմացեք գրանցված աշխատավարձը', id: v4() },
 ]
 
 export default function InputButtons() {
+
+    const [ backgroundColor, setBackgroundColor ] = useState({});
+
   return (
-    <Box>
+    <Box className={styles.container}>
         {
             inputTypes.map(e => (
                 <Button
-                    variant={"contained"}
+                    variant={"outlined"}
                     className={styles.button}
                     id={e.id}
+                    onClick={() => setBackgroundColor(e)}
+                    sx={{
+                        backgroundColor: backgroundColor?.id === e.id ? 'rgb(58, 93, 253)' : '',
+                        color: backgroundColor?.id === e.id ? '#FFF' : ''
+                }}
                 >
-                    {e.title}
+                    { e.title }
                 </Button>
             ))
         }
--- src/Components/PensionButtons/PensionButtons.jsx
+++ src/Components/PensionButtons/PensionButtons.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import { Grid, Button, Text } from '@nextui-org/react'
+import React, {useState} from 'react'
 import styles from "./PensionButtons.module.css"
+import {Box, Button, Typography} from "@mui/material";
+import {v4} from "uuid";
+
+const buttons = [
+    { title: 'ԱՅՈ, կամավոր միացած մինչ 07/2018-ը', id: v4() },
+    { title: 'ԱՅՈ , պարտադիր կամ 07/2018-ից հետո միացած կամավոր', id: v4() },
+    { title: 'ՈՉ, մասնակից չեմ', id: v4() },
+]
 
 export default function PensionButtons() {
+    const [ checkBtn, setCheckBtn ] = useState({});
   return (
-    <>
-        <Text h4 align="center" css={{padding:"3px"}}>
-            Մասնակցում եք արդյո՞ք պարտադիր կուտակային կենսաթոշակային համակարգին: Եթե այո, ապա ի՞նչ հիմունքներով:
-        </Text>
-        <Grid.Container className={styles.pension}>
-            <Grid>
-                <Button color="warning" auto ghost>
-                    <b>ԱՅՈ</b>, կամավոր միացած մինչ 07/2018-ը
-                </Button>
-            </Grid>
-            <Grid>
-                <Button color="warning" auto ghost>
-                    <b>ԱՅՈ</b> , պարտադիր կամ 07/2018-ից հետո միացած կամավոր
-                </Button>
-            </Grid>
-            <Grid>
-                <Button color="warning" auto ghost>
-                    <b>ՈՉ</b>, մասնակից չեմ   
-                </Button>
-            </Grid>    
-        </Grid.Container>
-    </>
+    <Box>
+        <Typography sx={{textAlign: 'center'}}>
+            Մասնակցում եք արդյո՞ք պարտադիր կուտակային կենսաթոշակային համակարգին:
+            Եթե այո, ապա ի՞նչ հիմունքներով:
+        </Typography>
+        <Box className={styles.container}>
+            {
+                buttons.map(el => (
+                    <Button
+                        id={el.id}
+                        variant={"outlined"}
+                        sx={{
+                            backgroundColor: checkBtn?.id === el.id ? 'orange' : '',
+                            color: checkBtn?.id === el.id ? '#FFF' : ''
+                        }}
+                        className={styles.button}
+                        onClick={ () => setCheckBtn( el ) }
+                    >{el.title}</Button>
+                ))
+            }
+        </Box>
+    </Box>
   )
 }
