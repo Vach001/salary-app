d0f4fc33033ae6d01ee17e28486523c8888b5c26
Vache Apinyan <apinyan.v@gmail.com>
2023-07-25 23:36:54 +0400

    feat(SalaryInput): add logic salary input

--- src/Components/SalaryInput/SalaryInput.jsx
+++ src/Components/SalaryInput/SalaryInput.jsx
@@ -1,10 +1,23 @@
 import { Grid, Input } from "@nextui-org/react";
-import React from "react";
+import React, { useState } from "react";
+import { initialState } from "../../constants/initialState.constants";
+
+let sal = initialState.sal;
 
 export default function SalaryInput() {
+  const [salary, setSalary] = useState('')
+
+  const handleChange = (e) => {
+    setSalary(e.target.value)
+  }
+  sal = salary
+  
   return (
     <Grid>
       <Input
+        value={salary}
+        onChange={handleChange}
+        type="number"
         bordered
         labelPlaceholder="Մուտքագրե՛ք Ձեր աշխատավարձի չափը"
         color="primary"
