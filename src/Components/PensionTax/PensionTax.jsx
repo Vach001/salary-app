78ed1405f7bb2ae5611a7a19157f31dcbb00badb
Vache Apinyan <apinyan.v@gmail.com>
2026-04-15 01:38:29 +0400

    style: add copy functionality to PensionTax (hover and click)

--- src/Components/PensionTax/PensionTax.jsx
+++ src/Components/PensionTax/PensionTax.jsx
@@ -1,23 +1,43 @@
-import React from "react";
-import { Card, Input } from "@nextui-org/react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { selectSalaryPensionTax } from "../../features/pensionTax/pensionTaxSlice";
+import styles from "./PensionTax.module.css";
 
 export default function PensionTax() {
-  const selectPensionTax = useSelector(selectSalaryPensionTax)
+  const pensionTax = useSelector(selectSalaryPensionTax);
+  const [copied, setCopied] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(pensionTax.toString());
+    setCopied(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    setCopied(false);
+  };
 
   return (
-    <Card
-      css={{
-        bgColor: "rgb(226, 246, 180, 0.5)",
-      }}
-    >
-      <Input 
-      rounded 
-      label="Սոցիալական վճար" 
-      value={selectPensionTax} 
-      color="primary" 
-      />
-    </Card>
+    <div className={styles.container}>
+      <div className={styles.leftBox}>
+        <span className={styles.leftText}>Սոցիալական վճար</span>
+      </div>
+
+      <div
+        onClick={handleCopy}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={handleMouseLeave}
+        className={styles.rightBox}
+      >
+        <span className={styles.rightText}>{pensionTax.toLocaleString()}</span>
+        {!copied && isHovered && (
+          <span className={styles.copyTooltip}>Պատճենել</span>
+        )}
+        {copied && isHovered && (
+          <span className={styles.copyTooltip}>Պատճենված է</span>
+        )}
+      </div>
+    </div>
   );
 }
