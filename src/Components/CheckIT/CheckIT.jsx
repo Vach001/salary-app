078452b2f9bde130f6f4d30b33edd757a93d8a05
Vache Apinyan <apinyan.v@gmail.com>
2026-04-15 01:25:19 +0400

    feat(src/Components/CheckIT): add check IT component styles

--- src/Components/CheckIT/CheckIT.jsx
+++ src/Components/CheckIT/CheckIT.jsx
@@ -3,6 +3,7 @@ import { Checkbox, Text } from "@nextui-org/react";
 import { useDispatch, useSelector } from "react-redux";
 import { setITCheckType, selectITCheckButton } from "../../features/iTCheckType/iTCheckTypeSlice";
 import { useTaxCalculations } from "../../hooks/useTaxCalculations";
+import styles from "./CheckIT.module.css";
 
 export default function CheckIT() {
   const selectITCheck = useSelector(selectITCheckButton);
@@ -18,17 +19,21 @@ export default function CheckIT() {
     const newValue = selectITCheck === "CERTIFIED" ? "UNCERTIFIED" : "CERTIFIED";
     dispatch(setITCheckType(newValue));
   };
+  const isChecked = selectITCheck === "CERTIFIED";
 
   return (
-    <Checkbox
-      css={{ display: "flex", justifyContent: "center", textAlign: "center" }}
-      color="gradient"
-      checked={selectITCheck === "CERTIFIED"}
-      onChange={handleCheckIT}
-    >
-      <Text h5>
-        Աշխատում եմ <b>ՏՏ</b> ոլորտի հարկային արտոնություններից օգտվող ընկերությունում
-      </Text>
-    </Checkbox>
+    <div className={styles.checkItContainer}>
+      <Checkbox
+        className={styles.checkItCheckbox}
+        size="lg"
+        color="primary"
+        checked={isChecked}
+        onChange={handleCheckIT}
+      >
+        <Text className={`${styles.checkItText} ${isChecked ? styles.checkedText : ""}`}>
+          Աշխատում եմ <b>ՏՏ</b> ոլորտի հարկային արտոնություններից օգտվող ընկերությունում
+        </Text>
+      </Checkbox>
+    </div>
   );
 }
