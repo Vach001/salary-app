f36d729cb1ac95db8bf8371d89b7bc1a0905565d
Vache Apinyan <apinyan.v@gmail.com>
2026-04-15 01:13:04 +0400

    feat(src/Components/IncomeTax.jsx): add copy on hover and click for income tax amount

--- src/Components/IncomeTax/IncomeTax.jsx
+++ src/Components/IncomeTax/IncomeTax.jsx
@@ -1,24 +1,48 @@
-import React from "react";
-import { Card, Input } from "@nextui-org/react";
-import { selectSalaryIncomeTax } from "../../features/incomeTax/incomeTaxSlice";
+import { useState } from "react";
 import { useSelector } from "react-redux";
+import { selectSalaryIncomeTax } from "../../features/incomeTax/incomeTaxSlice";
+import styles from "./IncomeTax.module.css";
 
 export default function IncomeTax() {
+  const incomeTax = useSelector(selectSalaryIncomeTax);
+  const isIT = useSelector(
+    (state) => state.iTCheckType?.iTCheckType === "CERTIFIED",
+  );
+  const [copied, setCopied] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
-  const selectIncomeTax = useSelector(selectSalaryIncomeTax)
+  const handleCopy = () => {
+    navigator.clipboard.writeText(incomeTax.toString());
+    setCopied(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    setCopied(false);
+  };
 
   return (
-    <Card
-      css={{
-        bgColor: "rgb(226, 246, 180, 0.5)",
-      }}
-    >
-      <Input 
-        rounded 
-        label="Եկամտային հարկ 20% / ՏՏ 10%" 
-        value={selectIncomeTax}
-        color="primary" 
-      />
-    </Card>
+    <div className={styles.container}>
+      <div className={styles.leftBox}>
+        <span className={styles.leftText}>
+          Եկամտային հարկ {isIT ? "10%" : "20%"}
+        </span>
+      </div>
+
+      <div
+        onClick={handleCopy}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={handleMouseLeave}
+        className={styles.rightBox}
+      >
+        <span className={styles.rightText}>{incomeTax.toLocaleString()}</span>
+        {!copied && isHovered && (
+          <span className={styles.copyTooltip}>Պատճենել</span>
+        )}
+        {copied && isHovered && (
+          <span className={styles.copyTooltip}>Պատճենված է</span>
+        )}
+      </div>
+    </div>
   );
 }
