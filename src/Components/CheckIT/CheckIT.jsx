8ce0ad863ff215ae1bc821405654196befcfc307
Vache Apinyan <apinyan.v@gmail.com>
2026-04-15 16:04:59 +0400

    style: update IT and Health Insurance checkbox to match styling

--- src/Components/CheckIT/CheckIT.jsx
+++ src/Components/CheckIT/CheckIT.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Checkbox, Text } from "@nextui-org/react";
+import { Text } from "@nextui-org/react";
 import { useDispatch, useSelector } from "react-redux";
 import { setITCheckType, selectITCheckButton } from "../../features/iTCheckType/iTCheckTypeSlice";
 import { useTaxCalculations } from "../../hooks/useTaxCalculations";
@@ -10,30 +10,27 @@ export default function CheckIT() {
   const dispatch = useDispatch();
   const { recalculateAll } = useTaxCalculations();
 
-  // Երբ iTCheckType-ը փոխվում է, վերահաշվարկել
   useEffect(() => {
     recalculateAll();
   }, [selectITCheck, recalculateAll]);
 
-  const handleCheckIT = () => {
-    const newValue = selectITCheck === "CERTIFIED" ? "UNCERTIFIED" : "CERTIFIED";
+  const handleCheckIT = (e) => {
+    const newValue = e.target.checked ? "CERTIFIED" : "UNCERTIFIED";
     dispatch(setITCheckType(newValue));
   };
+
   const isChecked = selectITCheck === "CERTIFIED";
 
   return (
     <div className={styles.checkItContainer}>
-      <Checkbox
-        className={styles.checkItCheckbox}
-        size="lg"
-        color="primary"
-        checked={isChecked}
-        onChange={handleCheckIT}
-      >
-        <Text className={`${styles.checkItText} ${isChecked ? styles.checkedText : ""}`}>
+      <label className={styles.checkItCheckbox}>
+        <input type="checkbox" checked={isChecked} onChange={handleCheckIT} />
+        <Text
+          className={`${styles.checkItText} ${isChecked ? styles.checkedText : ""}`}
+        >
           Աշխատում եմ <b>ՏՏ</b> ոլորտի հարկային արտոնություններից օգտվող ընկերությունում
         </Text>
-      </Checkbox>
+      </label>
     </div>
   );
 }
